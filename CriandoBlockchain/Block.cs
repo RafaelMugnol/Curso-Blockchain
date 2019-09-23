1e5ed70baf41f5634d19733ba7188a518b83c900
Aula Caxias do Sul <aulacx@ucs.local>
2019-09-23 11:33:57 -0300

    first commit

---
+++ CriandoBlockchain/Block.cs
@@ -0,0 +1,16 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CriandoBlockchain
+{
+    class Block
+    {
+        public int Index { get; set; }
+        public long Timestamp { get; set; }
+        public string Data { get; set; }
+        public string Hash { get; set; }
+        public string PreviousHash { get; set; }
+    }
+}
---
+++ CriandoBlockchain/Chain.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace CriandoBlockchain
+{
+    class Chain
+    {
+        public List<Block> ChainList { get; set; }
+
+        private static Chain instance = null;
+
+        public static Chain GetInstance()
+        {
+            if (instance == null)
+                instance = new Chain();
+
+            return instance;
+        }
+
+        private Chain()
+        {
+            var genesis = GetGenesisBlock();
+            this.ChainList = new List<Block>();
+            this.ChainList.Add(genesis);
+        }
+
+        private Block GetGenesisBlock() => new Block
+        {
+            Index = 0,
+            Hash = "213123123123",
+            PreviousHash = "",
+            Timestamp = 1465154705,
+            Data = "bloco 0"
+        };
+
+        public Block GetLatestBlock() => this.ChainList.Last();
+
+        public void AddBlock(Block block)
+        {
+            this.ChainList.Add(block);
+        }
+
+        private string Sha256(string value)
+        {
+            var sb = new StringBuilder();
+
+            using (var hash = SHA256.Create())
+            {
+                var enc = Encoding.UTF8;
+                var result = hash.ComputeHash(enc.GetBytes(value));
+
+                foreach (var b in result)
+                    sb.Append(b.ToString("x2"));
+            }
+
+            return sb.ToString();
+        }
+    }
+}
